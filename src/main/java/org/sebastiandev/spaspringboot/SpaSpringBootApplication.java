a4b27651ee33d91f3e7dde71b0ae83e52bd9bf59
Cauã Sebastian <133614515+Cauasebastian@users.noreply.github.com>
2024-08-12 23:07:38 -0300

    .

---
+++ src/main/java/org/sebastiandev/spaspringboot/SpaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.sebastiandev.spaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sebastiandev/spaspringboot/SpaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebastiandev.spaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
